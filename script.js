055b3ad87a19a824cbeb5cd4078e49bb50de33d4
Igor done <igor.done15@gmail.com>
2023-05-24 18:53:47 -0300

    adding drawing page

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 const chk = document.getElementById('chk')
+const additional = document.getElementById("add-card");
 
 chk.addEventListener('change', () => {
     document.body.classList.toggle('theme')
+});
+
+
+additional.style.cursor = 'pointer';
+
+additional.addEventListener('click', () => {
+    cardExtend();
+    additional.style.cursor = 'default';
+});
+
+function cardExtend() {
+    if(additional.style.width === ''){
+    additional.style.width = '100%';
+    }
+}
+
+additional.addEventListener('mouseleave', () => {
+    additional.style.width = '';
+    additional.style.cursor = 'pointer';
 });
